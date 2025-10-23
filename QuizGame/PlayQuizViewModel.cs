48d7e25f415aa3387ec2d9c5b4e3512a691bb617
GonsalsaLaptop <sebgon02@gmail.com>
2025-10-23 11:07:03 +0200

    Add project files.

---
+++ QuizGame/PlayQuizViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame
+{
+    public class PlayQuizViewModel
+    {
+        public Quiz Quiz { get; set; }
+        public Questions CurrentQuestion { get; set; }
+        public int SelectedAnswerIndex { get; set; }
+        public int CorrectAnswers { get; set; }
+        public int TotalAnswerd { get; set; }
+
+
+        public PlayQuizViewModel()
+        {
+            Quiz = new Quiz("TestQuiz");
+            Quiz.AddQuestion("Vad heter Sveriges huvudstad?", 0, "Stockholm", "Göteborg", "Malmö");
+            Quiz.AddQuestion("Vilken färg har himmlen?", 2, "Röd", "Grön", "Blå");
+            Quiz.AddQuestion("Hur många ben har en katt?", 1, "5", "4", "75");
+
+            CurrentQuestion = Quiz.GetRandomQuestion();
+            SelectedAnswerIndex = -1;
+        }
+
+        public void NextQuestion()
+        {
+            TotalAnswerd++;
+            if (CurrentQuestion.isCorrect(SelectedAnswerIndex))
+            {
+                CorrectAnswers++;
+            }
+
+            SelectedAnswerIndex = -1;
+            CurrentQuestion = Quiz.GetRandomQuestion();
+        }
+
+    }
+}
---
+++ QuizGame/Questions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame
+{
+    public class Questions
+    {
+        public string Statement { get; set; }
+        public string[] Answers { get; set; }
+        public int CorrectAnswer { get; set; }
+
+        public Questions(string statment, int correctanswer, params string[] answers)
+        {
+            Statement = statment;
+            Answers = answers;
+            CorrectAnswer = correctanswer;
+        }
+
+        public bool isCorrect(int selectedIndex)
+        {
+            return selectedIndex == CorrectAnswer;
+        }
+
+    }
+}
